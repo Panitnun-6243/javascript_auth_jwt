1d9a8b6ba5dba2b4d8e467e453530aa7d232d0ae
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-12-22 20:46:03 +0700

    refactor: add comment

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
+//app
 const express = require("express");
 const cors = require("cors");
 const app = express();
+//json body usage
 const bodyParser = require("body-parser");
 const jsonParser = bodyParser.json();
+//db
 const mysql = require("mysql2");
+//password hash
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
+//middleware
 app.use(cors());
 
 // create the connection to database
@@ -14,6 +21,7 @@ const connection = mysql.createConnection({
   database: "basic_authentication",
 });
 
+//register
 app.post("/register", jsonParser, function (req, res, next) {
   connection.execute(
     "INSERT INTO `users`(`email`, `password`, `fname`, `lname`) VALUES (?,?,?,?)",
@@ -28,6 +36,7 @@ app.post("/register", jsonParser, function (req, res, next) {
   );
 });
 
+//start server at port 5000_
 app.listen(5000, function () {
   console.log("Web server listening on port 5000");
 });
