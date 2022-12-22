5f5ed2b7efde1f750c236cfc2c6c895ceaf65e4d
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-12-22 20:40:06 +0700

    feat: add register response

--- server/server.js
+++ server/server.js
@@ -1,24 +1,33 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const bodyParser = require('body-parser')
-const jsonParser = bodyParser.json()
-const mysql = require('mysql2');
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json();
+const mysql = require("mysql2");
 
-app.use(cors())
+app.use(cors());
 
 // create the connection to database
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'basic_authentication'
+  host: "localhost",
+  user: "root",
+  database: "basic_authentication",
 });
 
-app.post('/register', jsonParser, function (req, res, next) {
-  const email = req.body.email
-  res.json({email})
-})
+app.post("/register", jsonParser, function (req, res, next) {
+  connection.execute(
+    "INSERT INTO `users`(`email`, `password`, `fname`, `lname`) VALUES (?,?,?,?)",
+    [req.body.email, req.body.password, req.body.fname, req.body.lname],
+    function (err, results) {
+      if (err) {
+        res.json({ status: "error", message: err });
+        return;
+      }
+      res.json({ status: "ok" });
+    }
+  );
+});
 
 app.listen(5000, function () {
-  console.log('Web server listening on port 5000')
-})
+  console.log("Web server listening on port 5000");
+});
