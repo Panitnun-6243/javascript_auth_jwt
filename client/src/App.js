046df8af14853769df97188ef0ab927b0f8bf9c4
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-12-28 09:27:57 +0700

    feat: add pages and router

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import RegisterPage from "./pages/registerPage";
+import LoginPage from "./pages/loginPage";
+import LandingPage from "./pages/landingPage";
+
 function App() {
-  return <div className="App">JWT</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<LoginPage />} />
+          <Route path="register" element={<RegisterPage />} />
+          <Route path="landing" element={<LandingPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
