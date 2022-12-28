7bbacba7333bd4ee68cfbcd4c8852e9c7a3ed7ad
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-12-28 11:02:56 +0700

    feat: create landing template

--- client/src/pages/landingPage.jsx
+++ client/src/pages/landingPage.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { Container, Typography, Grid, Button } from "@mui/material";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function LandingPage() {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/");
+  };
+
   return (
-    <div>LandingPage</div>
-  )
+    <Container component="main">
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        style={{ minHeight: "100vh" }}
+        rowSpacing={3}
+      >
+        <Grid item xs={2}>
+          <Typography variant="h4">You already logged in</Typography>
+        </Grid>
+        <Grid item>
+          <Button
+            size="large"
+            variant="contained"
+            color="success"
+            onClick={handleClick}
+          >
+            Logout
+          </Button>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
